6e273ffd7c948db16dac8260c82c3dace7fa7a4b
Gerasimos <acivil@hotmail.gr>
2023-01-13 15:34:28 +0200

    copied staggered to add pressure only

---
+++ tests/MGroup.DrugDeliveryModel.Tests/StaggeredSolutionPresIndep/Coupled7and9eqsSolutionex2.cs
@@ -0,0 +1,163 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MGroup.MSolve.Discretization.Entities;
+using MGroup.MSolve.Discretization.Dofs;
+using MGroup.MSolve.Numerics.Interpolation.Jacobians;
+using MGroup.DrugDeliveryModel.Tests.EquationModels;
+using MGroup.NumericalAnalyzers.Dynamic;
+using MGroup.NumericalAnalyzers.Logging;
+using MGroup.NumericalAnalyzers.Staggered;
+using MGroup.NumericalAnalyzers.Discretization.NonLinear;
+using MGroup.Constitutive.Structural;
+using MGroup.DrugDeliveryModel.Tests.Commons;
+using MGroup.NumericalAnalyzers;
+using MGroup.Solvers.Direct;
+using Xunit;
+using MGroup.Constitutive.ConvectionDiffusion;
+
+namespace MGroup.DrugDeliveryModel.Tests.Integration
+{
+	public class Coupled7and9eqsSolutionex2
+    {
+        const double Sc = 0.1;
+        const double timeStep = 1; // in days
+        const double totalTime = 10; // in days
+        static int incrementsPertimeStep = 1;
+
+        // strucutral model Loads
+        static StructuralDof loadedDof = StructuralDof.TranslationX;
+        static double load_value = 0.01;
+
+
+        //structural model properties
+        static double miNormal = 5; //KPa
+        static double kappaNormal = 6.667; //Kpa
+        static double miTumor = 22.44; //Kpa
+        static double kappaTumor = 216.7; //Kpa
+        static int currentTimeStep = 0;
+        static double lambda0 = 1;
+        int nGaussPoints = 1;
+        static double initial_dp_dx = 0; static double initial_dp_dy = 0; static double initial_dp_dz = 0;
+        static double velocityDivInitialVal = 0; 
+        static Dictionary<double, double[]> Solution = new Dictionary<double, double[]>(); private static List<(INode node, IDofType dof)> watchDofs = new List<(INode node, IDofType dof)>();
+
+        //Structural BCs . Not alla of these values are used but the ones used will be put here.
+        static double eq9modelMaxZ = 0.1;
+        static double eq9topValueprescribed = 1;
+        static double eq9modelMinZ = 0;
+        static double eq9bottomValueprescribed = 1;
+        static int eq9nodeIdToMonitor = 36;
+        static StructuralDof eq9dofTypeToMonitor = StructuralDof.TranslationX;
+
+        //Darcy model properties
+        static double Sv = 7e+3;// 1/(m)
+        static double k_th = 7.52e-13;// m2/(KPa sec)
+        static double pv = 4;// kPa
+        static double pl = 0;// KPa
+        static double Lp = 2.7e-9;// m/(KPa sec)
+        static double LplSvl = 3.75e-1;// 1/(KPa sec)
+        static double div_vs = 1e-6;// 1/(sec)
+        static int nodeIdToMonitor = 36;
+        static ConvectionDiffusionDof eq7n8dofTypeToMonitor = ConvectionDiffusionDof.UnknownVariable;
+        //Darcy of BCs
+        static double modelMaxZ=0.1;
+        static double topValueprescribed=1;
+        static double modelMinZ=0;
+        static double bottomValueprescribed=1;
+
+        public Coupled7and9eqsSolutionex2()
+		{
+            IsoparametricJacobian3D.DeterminantTolerance = 1e-20;
+        }
+
+        
+        [Theory]
+        //[InlineData("../../../DataFiles/workingTetMesh4886.mphtxt")]
+        //[InlineData("../../../DataFiles/chipMelter2M.mphtxt")]
+        //[InlineData("../../../DataFiles/MeshCyprusTM.mphtxt")]
+        [InlineData("../../../DataFiles/workingTetMesh155.mphtxt")]
+        public void MonophasicEquationModel(string fileName)
+		{
+            //Read geometry
+            var comsolReader = new ComsolMeshReader(fileName);
+
+            // initialize Shared quantities of Coupled model
+            Dictionary<int, double> lambda = new Dictionary<int, double>(comsolReader.ElementConnectivity.Count());
+            foreach (var elem in comsolReader.ElementConnectivity){lambda.Add(elem.Key,  lambda0);}
+            Dictionary<int, double[][]> pressureTensorDivergenceAtElementGaussPoints = new Dictionary<int, double[][]>(comsolReader.ElementConnectivity.Count());
+            foreach (var elem in comsolReader.ElementConnectivity)
+            {
+                var gpTensorDiv = new double[nGaussPoints][];
+                for (int i1 = 0; i1 < nGaussPoints; i1++) { gpTensorDiv[i1] = new double[] { initial_dp_dx, initial_dp_dy, initial_dp_dz }; }
+                pressureTensorDivergenceAtElementGaussPoints.Add(elem.Key, gpTensorDiv);
+            }
+            Dictionary<int, double[]> velocityDivergenceAtElementGaussPoints = new Dictionary<int, double[]>(comsolReader.ElementConnectivity.Count());
+            foreach (var elem in comsolReader.ElementConnectivity)
+            {
+                var velocityDiv = new double[nGaussPoints];
+                for (int i1 = 0; i1 < nGaussPoints; i1++) { velocityDiv[i1] = velocityDivInitialVal; }
+                velocityDivergenceAtElementGaussPoints.Add(elem.Key, velocityDiv);
+            }
+
+
+
+           var eq78model = new Eq78ModelProviderForStaggeredSolutionex2(comsolReader, k_th, Lp, Sv, pv, LplSvl, pl, velocityDivergenceAtElementGaussPoints, 
+                modelMaxZ, topValueprescribed, modelMinZ, bottomValueprescribed, nodeIdToMonitor, eq7n8dofTypeToMonitor);
+            var eq9model = new Eq9ModelProviderForStaggeredSolutionex2(comsolReader, Sc, miNormal, kappaNormal, miTumor, kappaTumor, timeStep, totalTime, lambda, pressureTensorDivergenceAtElementGaussPoints,
+                eq9modelMaxZ, eq9topValueprescribed, eq9modelMinZ, eq9bottomValueprescribed, eq9nodeIdToMonitor, eq9dofTypeToMonitor,loadedDof,load_value);
+
+
+            var equationModel = new Coupled7and9eqsModelex2(eq78model, eq9model, comsolReader, lambda,
+                pressureTensorDivergenceAtElementGaussPoints, velocityDivergenceAtElementGaussPoints, timeStep, totalTime, incrementsPertimeStep);
+
+
+            //var equationModel = new MonophasicEquationModel(fileName, Sc, miNormal, kappaNormal, miTumor, kappaTumor, timeStep, totalTime, lambda0);
+            var u1X = new double[(int)(totalTime / timeStep)];
+            var u1Y = new double[(int)(totalTime / timeStep)];
+            var u1Z = new double[(int)(totalTime / timeStep)];
+
+            var staggeredAnalyzer = new StepwiseStaggeredAnalyzer(equationModel.ParentAnalyzers, equationModel.ParentSolvers, equationModel.CreateModel, maxStaggeredSteps: 200, tolerance: 0.001);
+            for (currentTimeStep = 0; currentTimeStep < totalTime / timeStep; currentTimeStep++)
+            {
+                equationModel.CurrentTimeStep = currentTimeStep;
+                equationModel.CreateModelFirstTime(equationModel.ParentAnalyzers, equationModel.ParentSolvers);
+                staggeredAnalyzer.SolveCurrentStep();
+
+                // Edw ginetai access to antikeimeno LinearAnalyzerLogFactory tou loadcontrolAnalyzer pou kata th dhmiourgia tou to eixame perasei sto LogFactory
+                var allValues = ((DOFSLog)equationModel.ParentAnalyzers[0].ChildAnalyzer.Logs[0]).DOFValues.Select(x => x.val).ToArray();
+
+                u1X[currentTimeStep] = allValues[0];
+                //u1Y[currentTimeStep] = allValues[1];
+                //u1Z[currentTimeStep] = allValues[2];
+
+                if (Solution.ContainsKey(currentTimeStep))
+                {
+                    Solution[currentTimeStep] = allValues;
+                    Console.WriteLine($"Time step: {timeStep}");
+                    Console.WriteLine($"Displacement vector: {string.Join(", ", Solution[timeStep])}");
+                }
+                else
+                {
+                    Solution.Add(currentTimeStep, allValues);
+                }
+
+                for (int j = 0; j < equationModel.ParentAnalyzers.Length; j++)
+                {
+                    (equationModel.ParentAnalyzers[j] as PseudoTransientAnalyzer).AdvanceStep();
+                }
+
+                for (int j = 0; j < equationModel.ParentAnalyzers.Length; j++)
+                {
+                    equationModel.AnalyzerStates[j] = equationModel.ParentAnalyzers[j].CreateState();
+                    equationModel.NLAnalyzerStates[j] = equationModel.NLAnalyzers[j].CreateState();
+                }
+
+                Console.WriteLine($"Displacement vector: {string.Join(", ", Solution[currentTimeStep])}");
+            }
+        }
+
+        
+
+    }
+}
---
+++ tests/MGroup.DrugDeliveryModel.Tests/StaggeredSolutionPresIndep/Eq78ModelProviderForStaggeredSolutionex2.cs
@@ -0,0 +1,283 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MGroup.MSolve.Discretization.Entities;
+using MGroup.MSolve.Discretization.Dofs;
+using MGroup.MSolve.Numerics.Interpolation.Jacobians;
+using MGroup.DrugDeliveryModel.Tests.EquationModels;
+using MGroup.NumericalAnalyzers.Dynamic;
+using MGroup.NumericalAnalyzers.Logging;
+using MGroup.NumericalAnalyzers.Staggered;
+using MGroup.NumericalAnalyzers.Discretization.NonLinear;
+using MGroup.Constitutive.Structural;
+using MGroup.DrugDeliveryModel.Tests.Commons;
+using MGroup.NumericalAnalyzers;
+using MGroup.Solvers.Direct;
+using Xunit;
+using MGroup.Constitutive.ConvectionDiffusion;
+using MGroup.FEM.ConvectionDiffusion.Tests.Commons;
+using MGroup.MSolve.AnalysisWorkflow;
+using MGroup.MSolve.Solution;
+
+namespace MGroup.DrugDeliveryModel.Tests.Integration
+{
+	public class Eq78ModelProviderForStaggeredSolutionex2
+    {
+        private double Sv ;
+        private double k_th ;
+        private double pv ;
+        private double pl;
+        private double Lp ;
+        private double LplSvl ;
+        public Dictionary<int, double[]> div_vs {get; set;}
+        private int nodeIdToMonitor; //TODO put it where it belongs (coupled7and9eqsSolution.cs)
+        private ConvectionDiffusionDof dofTypeToMonitor;  
+        private ComsolMeshReader modelReader;
+
+        private double modelMaxZ;
+        private double topValueprescribed;
+        private double modelMinZ;
+        private double bottomValueprescribed;
+
+        public Eq78ModelProviderForStaggeredSolutionex2(ComsolMeshReader modelReader,
+            double kth, double Lp, double Sv, double pv, double LplSvl, double pl, Dictionary<int, double[]> div_vs,
+            double modelMaxZ, double topValueprescribed, double modelMinZ, double bottomValueprescribed,
+            int nodeIdToMonitor, ConvectionDiffusionDof dofTypeToMonitor )
+        {
+
+            this.Sv = Sv;
+            this.k_th = kth;
+            this.pv = pv;
+            this.pl = pl;
+            this.Lp = Lp; 
+            this.LplSvl = LplSvl;
+            this.div_vs = div_vs;
+            this.modelReader = modelReader;
+            IsoparametricJacobian3D.DeterminantTolerance = 1e-20;
+
+            //BCs 1
+            this.modelMaxZ = modelMaxZ;
+            this.topValueprescribed = topValueprescribed;
+            this.modelMinZ = modelMinZ;
+            this.bottomValueprescribed= bottomValueprescribed;
+
+            //log
+            this.nodeIdToMonitor = nodeIdToMonitor;
+            this.dofTypeToMonitor = dofTypeToMonitor;
+        }
+
+        public Model GetModel() //ORIGIN: SolveEquation7and8ofUpdatedReportStaticZeroPseudoTransientAnalyzer
+        {
+            //prosarmogh onomatwn metavltwn analoga tnn exisws tou word
+            double convectionCoeff = 0;
+            double capacity = 0;
+            double dependentProductionCoeff = -(Lp * Sv + LplSvl);
+            //double independentSource = Lp * Sv * pv + LplSvl * pl - div_vs;
+            double diffusion = k_th;
+
+            //  initiallize dictionaries of coefficients
+            Dictionary<int, double[]> ConvectionCoeffs = new Dictionary<int, double[]>();//=> new[]  {1d, 1d, 1d};               
+            //public double DiffusionCoeff;
+            Dictionary<int, double> DependentProductionCoeffs = new Dictionary<int, double>();
+            Dictionary<int, double> IndependentProductionCoeffs = new Dictionary<int, double>();
+            foreach (var elementConnectivity in modelReader.ElementConnectivity)
+            {
+                ConvectionCoeffs[elementConnectivity.Key] = new double[] { convectionCoeff, convectionCoeff, convectionCoeff };
+                DependentProductionCoeffs[elementConnectivity.Key] = dependentProductionCoeff;
+                var independentSource = Lp * Sv * pv + LplSvl * pl - div_vs[elementConnectivity.Key][0]; //TODO [0] is the first gauss point Make it more genreal for all guss paints
+                IndependentProductionCoeffs[elementConnectivity.Key] = independentSource;
+            }
+
+            //initialize mpdel provider solution
+            var modelProvider = new GenericComsol3DConvectionDiffusionProductionModelProviderDistributedSpace(modelReader);
+
+            var model = modelProvider.CreateModelFromComsolFile(ConvectionCoeffs, diffusion,
+                DependentProductionCoeffs, IndependentProductionCoeffs, capacity);
+
+            return model;
+
+        }
+
+        public void AddEq78ModelAppropriateBCs(Model model)
+        {
+            var modelProvider = new GenericComsol3DConvectionDiffusionProductionModelProviderDistributedSpace(modelReader);
+            modelProvider.AddTopAndBottomBCs(model, modelMaxZ, topValueprescribed, modelMinZ, bottomValueprescribed);
+        }
+
+        //TODO add linear analyzer logs px san to  loadControlAnalyzer.LogFactory = new LinearAnalyzerLogFactory(watchDofs[0], algebraicModel[0]); opws sto MonophasicEquationModel.cs
+        public (IParentAnalyzer analyzer, ISolver solver, IChildAnalyzer loadcontrolAnalyzer) GetAppropriateSolverAnalyzerAndLog(Model model, double pseudoTimeStep, double pseudoTotalTime, int currentStep, int nIncrements)
+        {
+            var solverFactory = new DenseMatrixSolver.Factory() { IsMatrixPositiveDefinite = false }; //Dense Matrix Solver solves with zero matrices!
+            var algebraicModel = solverFactory.BuildAlgebraicModel(model);
+            var solver = solverFactory.BuildSolver(algebraicModel);
+            var provider = new ProblemConvectionDiffusion(model, algebraicModel);
+
+            //var linearAnalyzer = new LinearAnalyzer(algebraicModel, solver, problem);
+            var loadControlAnalyzerBuilder = new LoadControlAnalyzer.Builder(algebraicModel, solver, provider, numIncrements: 2)
+            {
+                ResidualTolerance = 1E-8,
+                MaxIterationsPerIncrement = 100,
+                NumIterationsForMatrixRebuild = 1
+            };
+            var loadControlAnalyzer = loadControlAnalyzerBuilder.Build();
+
+            //List<(INode node, IDofType dof)> watchDofs = new List<(INode node, IDofType dof)>() { (model.GetNode(nodeIdToMonitor), dofTypeToMonitor) };
+            loadControlAnalyzer.TotalDisplacementsPerIterationLog = new TotalDisplacementsPerIterationLog(
+                new List<(INode node, IDofType dof)>()
+                {
+                    (model.NodesDictionary[nodeIdToMonitor], dofTypeToMonitor),
+
+                }, algebraicModel
+            );
+
+            //var staticAnalyzer = new StaticAnalyzer(algebraicModel, problem, loadControlAnalyzer);
+            var staticAnalyzer = (new PseudoTransientAnalyzer.Builder(algebraicModel, provider, loadControlAnalyzer, timeStep: pseudoTimeStep, totalTime: pseudoTotalTime, currentStep: currentStep)).Build();
+
+            //Sparse tet Mesh
+            var watchDofs = new[]
+            {
+                new List<(INode node, IDofType dof)>()
+                {
+                    (model.NodesDictionary[nodeIdToMonitor], dofTypeToMonitor),
+                    //(model[0].NodesDictionary[333], StructuralDof.TranslationY),
+                    //(model[0].NodesDictionary[333], StructuralDof.TranslationZ),
+                }
+            };
+
+
+            //Print the coordinates of the watchdof for comsol comparison
+            //Console.WriteLine("DOF :" + model[0].NodesDictionary[13].ID + " X: " + model[0].NodesDictionary[13].X + " Y: " + model[0].NodesDictionary[13].Y + " Z: " + model[0].NodesDictionary[13].Z + " Time: " + currentTimeStep * timeStep + " lambda: " + lambda[13]);
+            //Console.WriteLine("DOF :" + model[0].NodesDictionary[333].ID + " X: " + model[0].NodesDictionary[333].X + " Y: " + model[0].NodesDictionary[333].Y + " Z: " + model[0].NodesDictionary[333].Z + " Time: " + currentTimeStep * timeStep + " lambda: " + lambda[333]);
+
+            loadControlAnalyzer.LogFactory = new LinearAnalyzerLogFactory(watchDofs[0], algebraicModel);
+
+
+            return (staticAnalyzer, solver, loadControlAnalyzer);
+
+        }
+
+
+        public void SolveEquation7and8ofUpdatedReportStaticZeroPseudoTransientAnalyzer(string fileName,
+            double kth, double Lp, double Sv, double pv, double LplSvl, double pl, double div_vs)
+        {
+            double pseudoTotalTime = 2;
+            double pseudoTimeStep = 1;
+            double currentPseudoTimeStep = 1;
+
+            //prosarmogh onomatwn metavltwn analoga tnn exisws tou word
+            double convectionCoeff = 0;
+            double capacity = 0;
+            double dependentProductionCoeff = -(Lp * Sv + LplSvl);
+            double independentSource = Lp * Sv * pv + LplSvl * pl - div_vs;
+            double diffusion = kth;
+
+            //get element connectivities from file
+            var modelReader = new ComsolMeshReader(fileName);
+
+            //  initiallize dictionaries of coefficients
+            Dictionary<int, double[]> ConvectionCoeffs = new Dictionary<int, double[]>();//=> new[]  {1d, 1d, 1d};               
+            //public double DiffusionCoeff;
+            Dictionary<int, double> DependentProductionCoeffs = new Dictionary<int, double>();
+            Dictionary<int, double> IndependentProductionCoeffs = new Dictionary<int, double>();
+            foreach (var elementConnectivity in modelReader.ElementConnectivity)
+            {
+                ConvectionCoeffs[elementConnectivity.Key] = new double[] { convectionCoeff, convectionCoeff, convectionCoeff };
+                DependentProductionCoeffs[elementConnectivity.Key] = dependentProductionCoeff;
+                IndependentProductionCoeffs[elementConnectivity.Key] = independentSource;
+            }
+
+
+            //initialize mpdel provider solution
+            var modelProvider = new GenericComsol3DConvectionDiffusionProductionModelProviderDistributedSpace(modelReader);
+
+            var model = modelProvider.CreateModelFromComsolFile(ConvectionCoeffs, diffusion,
+                DependentProductionCoeffs, IndependentProductionCoeffs, capacity);
+
+
+            //////////////////////////////////////BCs ed
+            modelProvider.AddTopAndBottomBCs(model, 0.1, 1, 0, 1);
+            //modelProvider.AddInitialConditionsForTheRestOfBulkNodes(model, 0.1, 0, 1);
+
+
+
+
+
+            var solverFactory = new DenseMatrixSolver.Factory() { IsMatrixPositiveDefinite = false }; //Dense Matrix Solver solves with zero matrices!
+            var algebraicModel = solverFactory.BuildAlgebraicModel(model);
+            var solver = solverFactory.BuildSolver(algebraicModel);
+            var provider = new ProblemConvectionDiffusion(model, algebraicModel);
+
+            //var linearAnalyzer = new LinearAnalyzer(algebraicModel, solver, problem);
+            var loadControlAnalyzerBuilder = new LoadControlAnalyzer.Builder(algebraicModel, solver, provider, numIncrements: 2)
+            {
+                ResidualTolerance = 1E-8,
+                MaxIterationsPerIncrement = 100,
+                NumIterationsForMatrixRebuild = 1
+            };
+            var loadControlAnalyzer = loadControlAnalyzerBuilder.Build();
+
+            List<(INode node, IDofType dof)> watchDofs = new List<(INode node, IDofType dof)>() { (model.GetNode(nodeIdToMonitor), dofTypeToMonitor) };
+            loadControlAnalyzer.TotalDisplacementsPerIterationLog = new TotalDisplacementsPerIterationLog(
+                new List<(INode node, IDofType dof)>()
+                {
+                    (model.NodesDictionary[nodeIdToMonitor], dofTypeToMonitor),
+
+                }, algebraicModel
+            );
+
+            //var staticAnalyzer = new StaticAnalyzer(algebraicModel, problem, loadControlAnalyzer);
+            var staticAnalyzer = (new PseudoTransientAnalyzer.Builder(algebraicModel, provider, loadControlAnalyzer, timeStep: pseudoTimeStep, totalTime: pseudoTotalTime, currentStep: 0)).Build();
+
+
+            //Firtst step
+            staticAnalyzer.Initialize();
+            (staticAnalyzer as IStepwiseAnalyzer).Solve();
+
+            //var allValues = ((DOFSLog)staticAnalyzer.ChildAnalyzer.Logs[0]).DOFValues.Select(x => x.val).ToArray();
+
+            var AnalyzerStates = (staticAnalyzer as IAnalyzer).CreateState();
+            var NLAnalyzerStates  = (loadControlAnalyzer as IAnalyzer).CreateState();
+
+
+            //second step
+            (staticAnalyzer as PseudoTransientAnalyzer).AdvanceStep();
+
+            // TODO STHN CREATE MODEL TA KANEI OLA new apo tn arxh na
+            // to kanoume model apo tn arxh giafto tous pernaei ta states.
+
+
+            provider.Reset();
+            staticAnalyzer.Initialize();
+            (staticAnalyzer as IAnalyzer).CurrentState = AnalyzerStates;
+            (loadControlAnalyzer as IAnalyzer).CurrentState = NLAnalyzerStates;
+
+            (staticAnalyzer as IStepwiseAnalyzer).Solve();
+            var allValues2 = ((DOFSLog)staticAnalyzer.ChildAnalyzer.Logs[0]).DOFValues.Select(x => x.val).ToArray();
+
+
+
+
+
+            List<double> displacements = new List<double>();
+
+            for (var iter = 0; iter < 3; ++iter)
+            {
+                for (var i = 0; i < 1; ++i)
+                {
+                    displacements.Add(loadControlAnalyzer.TotalDisplacementsPerIterationLog.GetTotalDisplacement(iter, model.NodesDictionary[nodeIdToMonitor], dofTypeToMonitor));
+
+
+                }
+            }
+            var expected = new double[] { -0.068261820555808483 };
+            double tolerance = 1E-6;
+            Assert.True(ResultChecker.CheckResults(new double[] { displacements.Last() }, expected, tolerance));
+
+
+        }
+
+
+
+
+    }
+}
